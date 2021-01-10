19c1b1d611d8ea53bb528e315df8a2493af18afa
tunywalker <63519095+tunywalker@users.noreply.github.com>
2021-01-10 04:42:18 +0300

    Proje dosyası ekle.

---
+++ Otopark Otomasyonu/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Otopark_Otomasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picLicensePlate = new System.Windows.Forms.PictureBox();
+            this.lbxPlates = new System.Windows.Forms.ListBox();
+            this.picOriginal = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLicensePlate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.picLicensePlate);
+            this.tabPage1.Controls.Add(this.lbxPlates);
+            this.tabPage1.Controls.Add(this.picOriginal);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.ImageIndex = 1;
+            this.tabPage1.ImeMode = System.Windows.Forms.ImeMode.HangulFull;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(590, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(184, 18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(18, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 27;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(257, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Bulunan Plakalar:";
+            // 
+            // picLicensePlate
+            // 
+            this.picLicensePlate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picLicensePlate.Location = new System.Drawing.Point(18, 297);
+            this.picLicensePlate.Name = "picLicensePlate";
+            this.picLicensePlate.Size = new System.Drawing.Size(184, 44);
+            this.picLicensePlate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLicensePlate.TabIndex = 11;
+            this.picLicensePlate.TabStop = false;
+            // 
+            // lbxPlates
+            // 
+            this.lbxPlates.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbxPlates.FormattingEnabled = true;
+            this.lbxPlates.ItemHeight = 17;
+            this.lbxPlates.Location = new System.Drawing.Point(260, 252);
+            this.lbxPlates.Name = "lbxPlates";
+            this.lbxPlates.Size = new System.Drawing.Size(161, 89);
+            this.lbxPlates.TabIndex = 13;
+            // 
+            // picOriginal
+            // 
+            this.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picOriginal.Location = new System.Drawing.Point(8, 6);
+            this.picOriginal.Name = "picOriginal";
+            this.picOriginal.Size = new System.Drawing.Size(211, 162);
+            this.picOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOriginal.TabIndex = 12;
+            this.picOriginal.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 18);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Plaka Resmi:";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(598, 450);
+            this.tabControl1.TabIndex = 29;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(280, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "Deneme";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLicensePlate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picOriginal)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox picLicensePlate;
+        private System.Windows.Forms.ListBox lbxPlates;
+        private System.Windows.Forms.PictureBox picOriginal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Otopark Otomasyonu/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO;
+using openalprnet;
+
+using System.Drawing.Imaging;
+
+namespace Otopark_Otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                var uri = new UriBuilder(codeBase);
+                var path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public Rectangle boundingRectangle(List<Point> points)
+        {
+            var minX = points.Min(p => p.X);
+            var minY = points.Min(p => p.Y);
+            var maxX = points.Max(p => p.X);
+            var maxY = points.Max(p => p.Y);
+            return new Rectangle(new Point(minX, minY), new Size(maxX - minX, maxY - minY));
+        }
+
+        private static Image cropImage(Image img, Rectangle cropArea)
+        {
+            var bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+        private static Image cropImage2(Image img)
+        {
+            var bmpImage = new Bitmap(img);
+            return bmpImage;
+        }
+
+        public static Bitmap combineImages(List<Image> images)
+        {
+
+            Bitmap finalImage = null;
+
+            try
+            {
+                var width = 0;
+                var height = 0;
+
+                foreach (var bmp in images)
+                {
+                    width += bmp.Width;
+                    height = bmp.Height > height ? bmp.Height : height;
+                }
+
+                finalImage = new Bitmap(width, height);
+
+                using (var g = Graphics.FromImage(finalImage))
+                {
+
+                    g.Clear(Color.Black);
+
+                    var offset = 0;
+                    foreach (Bitmap image in images)
+                    {
+                        g.DrawImage(image,
+                                    new Rectangle(offset, 0, image.Width, image.Height));
+                        offset += image.Width;
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+              
+
+                throw ex;
+            }
+            finally
+            {
+                
+                foreach (var image in images)
+                {
+                    image.Dispose();
+                  
+                }
+            }
+        }
+       
+        public string processImageFile(string fileName)
+        {
+
+        
+            var region = "eu";
+            String config_file = Path.Combine(AssemblyDirectory, "openalpr.conf");
+            String runtime_data_dir = Path.Combine(AssemblyDirectory, "runtime_data");
+
+            using (var alpr = new AlprNet(region, config_file, runtime_data_dir))
+            {
+
+
+                var results = alpr.Recognize(fileName);
+
+                var images = new List<Image>(results.Plates.Count());
+                var i = 1;
+                if (results.Plates.Count() == 0)
+                {
+                    picOriginal.ImageLocation = fileName;
+                    picOriginal.Load();
+                }
+                foreach (var result in results.Plates)
+                {
+                    var rect = boundingRectangle(result.PlatePoints);                  
+                    var img = Image.FromFile(fileName);
+                    var cropped = cropImage(img, rect);               
+                    picOriginal.Image = cropImage2(img);
+                    img.Dispose();
+                    images.Add(cropped);
+                    foreach (var plate in result.TopNPlates)
+                    {
+                        lbxPlates.Items.Add(plate.Characters);
+                        break;
+                    }
+
+                }
+
+                if (images.Any())
+                {
+
+                    picLicensePlate.Image = combineImages(images);
+                }
+
+            }
+          
+            return "";
+        }
+      
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            processImageFile("c:/plaka/ornekPlaka.jpg");
+        }
+    }
+}

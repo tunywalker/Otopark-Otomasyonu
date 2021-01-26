d5e7dc7952f3bde9d6df2f640a71ef078b70f25b
tunywalker <63519095+tunywalker@users.noreply.github.com>
2021-01-27 00:33:08 +0300

    İp Kamera ile görüntü Girişi Plaka Tanıma işlemi

--- Otopark Otomasyonu/Form1.Designer.cs
+++ Otopark Otomasyonu/Form1.Designer.cs
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.picLicensePlate = new System.Windows.Forms.PictureBox();
@@ -36,7 +38,7 @@
             this.picOriginal = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picLicensePlate)).BeginInit();
@@ -55,18 +57,32 @@
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.ImageIndex = 1;
             this.tabPage1.ImeMode = System.Windows.Forms.ImeMode.HangulFull;
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(590, 424);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(789, 525);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(637, 95);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 28;
+            this.button4.Text = "Deneme";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(184, 18);
+            this.pictureBox2.Location = new System.Drawing.Point(228, 28);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(18, 30);
+            this.pictureBox2.Size = new System.Drawing.Size(51, 30);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 27;
             this.pictureBox2.TabStop = false;
@@ -76,38 +92,43 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(257, 231);
+            this.label3.Location = new System.Drawing.Point(324, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 18);
+            this.label3.Size = new System.Drawing.Size(155, 24);
             this.label3.TabIndex = 16;
             this.label3.Text = "Bulunan Plakalar:";
             // 
             // picLicensePlate
             // 
             this.picLicensePlate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.picLicensePlate.Location = new System.Drawing.Point(18, 297);
+            this.picLicensePlate.Location = new System.Drawing.Point(24, 366);
+            this.picLicensePlate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picLicensePlate.Name = "picLicensePlate";
-            this.picLicensePlate.Size = new System.Drawing.Size(184, 44);
+            this.picLicensePlate.Size = new System.Drawing.Size(245, 54);
             this.picLicensePlate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picLicensePlate.TabIndex = 11;
             this.picLicensePlate.TabStop = false;
+            this.picLicensePlate.Visible = false;
             // 
             // lbxPlates
             // 
             this.lbxPlates.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lbxPlates.FormattingEnabled = true;
-            this.lbxPlates.ItemHeight = 17;
-            this.lbxPlates.Location = new System.Drawing.Point(260, 252);
+            this.lbxPlates.ItemHeight = 22;
+            this.lbxPlates.Location = new System.Drawing.Point(328, 60);
+            this.lbxPlates.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lbxPlates.Name = "lbxPlates";
-            this.lbxPlates.Size = new System.Drawing.Size(161, 89);
+            this.lbxPlates.Size = new System.Drawing.Size(213, 92);
             this.lbxPlates.TabIndex = 13;
             // 
             // picOriginal
             // 
             this.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.picOriginal.Location = new System.Drawing.Point(8, 6);
+            this.picOriginal.Location = new System.Drawing.Point(11, 7);
+            this.picOriginal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.picOriginal.Name = "picOriginal";
-            this.picOriginal.Size = new System.Drawing.Size(211, 162);
+            this.picOriginal.Size = new System.Drawing.Size(281, 199);
             this.picOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picOriginal.TabIndex = 12;
             this.picOriginal.TabStop = false;
@@ -116,40 +137,38 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(26, 276);
+            this.label2.Location = new System.Drawing.Point(35, 340);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(96, 18);
+            this.label2.Size = new System.Drawing.Size(118, 24);
             this.label2.TabIndex = 15;
             this.label2.Text = "Plaka Resmi:";
+            this.label2.Visible = false;
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(598, 450);
+            this.tabControl1.Size = new System.Drawing.Size(797, 554);
             this.tabControl1.TabIndex = 29;
             // 
-            // button4
+            // timer1
             // 
-            this.button4.Location = new System.Drawing.Point(336, 101);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 28;
-            this.button4.Text = "Deneme";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(625, 450);
+            this.ClientSize = new System.Drawing.Size(833, 554);
             this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Otopark Otomasyonu";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -173,6 +192,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Otopark Otomasyonu/Form1.cs
+++ Otopark Otomasyonu/Form1.cs
@@ -10,17 +10,32 @@ using System.Windows.Forms;
 using System.Reflection;
 using System.IO;
 using openalprnet;
-
+using AForge.Video;  //Referansları ekliyoruz
+using AForge.Video.DirectShow; //Referansları ekliyoruz
 using System.Drawing.Imaging;
 
 namespace Otopark_Otomasyonu
 {
+
     public partial class Form1 : Form
     {
+        JPEGStream stream;
         public Form1()
         {
+            stream = new JPEGStream("http://192.168.1.104:8080/photo.jpg");
+            //http://192.168.1.5:8080/shot.jpg?rnd=660075
+          
+            InitializeComponent();
+            stream.NewFrame += stream_NewFrame;
             InitializeComponent();
         }
+        void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox2.Image = bmp;
+        }
+        private FilterInfoCollection webcam;//webcam isminde tanımladığımız değişken bilgisayara kaç kamera bağlıysa onları tutan bir dizi. 
+        private VideoCaptureDevice cam;//cam ise bizim kullanacağımız aygıt.
         public static string AssemblyDirectory
         {
             get
@@ -90,25 +105,25 @@ namespace Otopark_Otomasyonu
             {
                 if (finalImage != null)
                     finalImage.Dispose();
-              
+
 
                 throw ex;
             }
             finally
             {
-                
+
                 foreach (var image in images)
                 {
                     image.Dispose();
-                  
+
                 }
             }
         }
-       
+
         public string processImageFile(string fileName)
         {
 
-        
+
             var region = "eu";
             String config_file = Path.Combine(AssemblyDirectory, "openalpr.conf");
             String runtime_data_dir = Path.Combine(AssemblyDirectory, "runtime_data");
@@ -120,7 +135,7 @@ namespace Otopark_Otomasyonu
                 var results = alpr.Recognize(fileName);
 
                 var images = new List<Image>(results.Plates.Count());
-                var i = 1;
+                //    var i = 1;
                 if (results.Plates.Count() == 0)
                 {
                     picOriginal.ImageLocation = fileName;
@@ -128,9 +143,9 @@ namespace Otopark_Otomasyonu
                 }
                 foreach (var result in results.Plates)
                 {
-                    var rect = boundingRectangle(result.PlatePoints);                  
+                    var rect = boundingRectangle(result.PlatePoints);
                     var img = Image.FromFile(fileName);
-                    var cropped = cropImage(img, rect);               
+                    var cropped = cropImage(img, rect);
                     picOriginal.Image = cropImage2(img);
                     img.Dispose();
                     images.Add(cropped);
@@ -145,24 +160,63 @@ namespace Otopark_Otomasyonu
                 if (images.Any())
                 {
 
-                    picLicensePlate.Image = combineImages(images);
+                 //   picLicensePlate.Image = combineImages(images); PLakayı kes
                 }
 
             }
-          
+
             return "";
         }
-      
-     
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            stream.Start();
+            timer1.Interval = 3000;
+            timer1.Start();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             processImageFile("c:/plaka/ornekPlaka.jpg");
             //Deneme
+        }
+
+        int durum = 1;
+        int timer = 0;
+        Random rnd = new Random();
+        int rastgele;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer++;
+            rastgele = rnd.Next(1, 10000);
+            if (true)
+            {
+
+
+                if (pictureBox2.Image != null)
+                {
+                    if (timer % 10 == 0)
+                    {
+                        lbxPlates.Items.Clear();
+                    }
+                    
+                    Bitmap varBmp = new Bitmap(pictureBox2.Image);
+                    Bitmap newBitmap = new Bitmap(varBmp);
+                    //processImageFile("C:/plaka/SavedImage.jpg");
+                    varBmp.Save(@"C:\plaka\"+rastgele+".png", ImageFormat.Png);
+                    //Now Dispose to free the memory
+                    varBmp.Dispose();
+                    varBmp = null;
+
+                    processImageFile("C:/plaka/"+rastgele+".png");
+                }
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
